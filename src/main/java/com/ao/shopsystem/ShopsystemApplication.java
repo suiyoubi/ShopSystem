e57bb2c689993e870d4560cb9b20ec0d1afbb08a
suiyoubi <mike.tang96@gmail.com>
2018-09-20 22:36:41 -0700

    First Commit, Connect the application to the database

---
+++ src/main/java/com/ao/shopsystem/ShopsystemApplication.java
@@ -0,0 +1,12 @@
+package com.ao.shopsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopsystemApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ao/shopsystem/entity/Item.java
@@ -0,0 +1,26 @@
+package com.ao.shopsystem.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * TODO: Add description Here.
+ * Created by ao on 2018-09-20
+ */
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false, unique = true)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(255) DEFAULT ''")
+    private String name;
+
+    @Column(nullable = false, columnDefinition = "BIGINT DEFAULT 0")
+    private Long price;
+}
